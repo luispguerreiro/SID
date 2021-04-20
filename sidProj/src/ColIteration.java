0c0fcf8bd834162fc158ff2454d01953a694e366
hmpms-iscte <hmpms@iscte-iul.pt>
2021-04-20 17:52:31 +0100

    ...s

--- sidProj/src/ColIteration.java
+++ sidProj/src/ColIteration.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bson.Document;
 
 import com.mongodb.BasicDBObject;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Sorts;
 
 public class ColIteration implements Runnable {
 
@@ -35,11 +41,27 @@ public class ColIteration implements Runnable {
 
 	@Override
 	public void run() {
-		MongoCursor<Document> cursor = colOrigin.find().iterator();
-		colDestin.drop();
-		BasicDBObject document = new BasicDBObject();
+		MongoCursor<Document> cursorFirst = colDestin.find().iterator();
+		FindIterable<Document> doc1 = colDestin.find().sort(Sorts.descending("Data"));
+//		System.out.println(doc1.first().get("_id"));
+		Document o = doc1.first();
+		if (o != null) {
+			o.toString();
+
+//		FindIterable<Document> currentVersionDocumentIterable =   
+//		    colDestin.find().sort(new Document("_id", -1)).limit(1);
+//		System.out.println(colDestin.find().sort(new Document("_id", -1)).limit(1));
+
+			List<Document> l1 = colOrigin.find(Filters.gte("_id", o.get("_id"))).into(new ArrayList<>());
+			for (Document document : l1) {
+				System.out.println("ol2");
+				System.out.println(document.toJson());
+			}
+		}
+//		colDestin.drop();
+			BasicDBObject document = new BasicDBObject();
+			MongoCursor<Document> cursor = colOrigin.find().iterator();
 		colDestin.deleteMany(document);
-		
 		Document doc = new Document();
 		while (true) {
 			doc = cursor.next();
@@ -47,12 +69,14 @@ public class ColIteration implements Runnable {
 			row = doc.toJson();
 			t = separateDate(row);
 			doc.clear();
-			System.out.println(doc.parse(t));
+//			System.out.println(doc.parse(t));
 //			Connections.colDestin.insertOne(doc.parse(t));
 			colDestin.insertOne(doc.parse(t));
-			
-		}
-		
-	}
 
+		}
+
+	}
+	
 }
+
+
