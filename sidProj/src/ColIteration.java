93f4e283a28ba84524183ea4fa4dcad45b9a5ec1
hmpms-iscte <hmpms@iscte-iul.pt>
2021-04-21 16:42:12 +0100

    mongo

--- sidProj/src/ColIteration.java
+++ sidProj/src/ColIteration.java
@@ -42,41 +42,45 @@ public class ColIteration implements Runnable {
 	@Override
 	public void run() {
 		MongoCursor<Document> cursorFirst = colDestin.find().iterator();
-		FindIterable<Document> doc1 = colDestin.find().sort(Sorts.descending("Data"));
-//		System.out.println(doc1.first().get("_id"));
-		Document o = doc1.first();
-		if (o != null) {
-			o.toString();
+		FindIterable<Document> doc1 = colDestin.find().sort(Sorts.descending("_id"));
+		Document last = doc1.first();
+		if (last != null) {
+			last.toString();
+			System.out.println(last.get("_id"));
 
-//		FindIterable<Document> currentVersionDocumentIterable =   
-//		    colDestin.find().sort(new Document("_id", -1)).limit(1);
-//		System.out.println(colDestin.find().sort(new Document("_id", -1)).limit(1));
-
-			List<Document> l1 = colOrigin.find(Filters.gt("_id", o.get("_id"))).into(new ArrayList<>());
+			List<Document> l1 = colOrigin.find(Filters.gt("_id", last.get("_id"))).into(new ArrayList<>());
 			for (Document document : l1) {
-				System.out.println(document.toJson());
-				colDestin.insertOne(document);
+				String row, t = "";
+				row = document.toJson();
+				t = separateDate(row);
+				document.clear();
+				colDestin.insertOne(document.parse(t));
+				System.out.println(document.parse(t));
 			}
 		}
-//		colDestin.drop();
-			BasicDBObject document = new BasicDBObject();
-			MongoCursor<Document> cursor = colOrigin.find().iterator();
-		colDestin.deleteMany(document);
-		Document doc = new Document();
+
+		FindIterable<Document> doc2 = colDestin.find().sort(Sorts.descending("_id"));
+		Document lastBeforeUpdate = doc2.first();
+
+		MongoCursor<Document> cursor = colOrigin.find((Filters.gt("_id", lastBeforeUpdate.get("_id")))).iterator();
+
 		while (true) {
-			doc = cursor.next();
-			String row, t = "";
-			row = doc.toJson();
-			t = separateDate(row);
-			doc.clear();
-			System.out.println("aqui");
-//			Connections.colDestin.insertOne(doc.parse(t));
-			colDestin.insertOne(doc.parse(t));
+			FindIterable<Document> doc3 = colDestin.find().sort(Sorts.descending("_id"));
+			Document doc = doc3.first();
+
+			cursor = colOrigin.find((Filters.gt("_id", doc.get("_id")))).iterator();
+			if (cursor.hasNext()) {
+				doc = cursor.next();
+				String row, t = "";
+				row = doc.toJson();
+				t = separateDate(row);
+				doc.clear();
+				colDestin.insertOne(doc.parse(t));
+				System.out.println("--->" + doc.parse(t));
+			}
 
 		}
 
 	}
-	
+
 }
-
-
